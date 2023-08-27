3735c71513f3d9442496f92a50da88275e2654a6
tahirivazquez <tahiwdev@gmail.com>
2023-08-27 19:46:27 -0300

    alerta div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function (){
+    const elDiv = document.getElementById("elDiv");
+
+    elDiv.addEventListener("click", function (){
+        alert("Hola! Soy el div")
+    });
+});
